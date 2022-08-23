6ab50b8bdb0425966498324b29e87517e1111be5
100044 <100044@jashu>
2022-08-23 21:35:20 +0530

    uploading the new project to github

---
+++ src/main/java/june_maven_2022/june_maven_2022/App.java
@@ -0,0 +1,13 @@
+package june_maven_2022.june_maven_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
